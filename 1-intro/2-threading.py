df61b14222fe6124265fc43508d4d30404816e2c
V1M <nene.anantakul@gmail.com>
2025-06-11 10:25:22 +0700

    Add time stamp of burger

--- 1-intro/2-threading.py
+++ 1-intro/2-threading.py
@@ -3,6 +3,9 @@ import threading
 from datetime import datetime
 
 def make_burger(student_id):
+    start_time = datetime.now()
+    start = time.time()
+
     print(f"[{datetime.now().strftime('%H:%M:%S')}] เริ่มทำเบอร์เกอร์ให้นักเรียนคนที่ {student_id}")
     
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 1. ทอดเบอร์เกอร์...")
@@ -17,7 +20,10 @@ def make_burger(student_id):
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 4. ห่อเบอร์เกอร์...")
     time.sleep(5)
 
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id}")
+    end_time = datetime.now()
+    end = time.time()
+
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id} เริ่มตอน [{start_time.strftime('%H:%M:%S')}] จบตอน [{end_time.strftime('%H:%M:%S')}] ระยะเวลา {end-start:.2f}        " )
 
 def main():
     start = time.time()
