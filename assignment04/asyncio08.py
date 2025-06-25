65929688af7e01fe19ca15ecb08280d8b24d27fc
V1M <nene.anantakul@gmail.com>
2025-06-25 12:11:03 +0700

    Finish Async8

--- assignment04/asyncio08.py
+++ assignment04/asyncio08.py
@@ -1,1 +1,15 @@
 # get Exception
+import asyncio
+
+async def error_task() :
+    await asyncio.sleep(1)
+    return ValueError("ERROR")
+
+async def main() :
+    task = asyncio.create_task(error_task())
+    try:
+        await task
+    except Exception :
+        print("Exception : ", task.exception())
+
+asyncio.run(main())
