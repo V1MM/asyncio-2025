3a49b79075039fba10fc40031f4088ddf127aed7
V1M <nene.anantakul@gmail.com>
2025-06-25 12:10:13 +0700

    Finish Async3

--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,17 @@
 # Create 2 Tasks with High-Level API
+import asyncio
 
+async def dowload_image(name, delay) :
+    print(f'{name} Downloading...')
+    await asyncio.sleep(delay)
+    print(f'{name} Finish!')
+
+async def main():
+    task1 = asyncio.create_task(dowload_image("Image 1", 5))
+    task2 = asyncio.create_task(dowload_image("Image 2", 3))
+
+    await task1
+    await task2
+
+asyncio.run(main() )
+                                 
