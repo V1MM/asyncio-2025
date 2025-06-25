261df3273de6cfdc7f9a175ee53e65b3971f36a8
V1M <nene.anantakul@gmail.com>
2025-06-25 12:10:30 +0700

    Finish Async7

--- assignment04/asyncio07.py
+++ assignment04/asyncio07.py
@@ -1,1 +1,13 @@
 # get result
+import asyncio
+
+async def simple_task() :
+    await asyncio.sleep(1)
+    return "Finish"
+
+async def main() :
+    task = asyncio.create_task(simple_task())
+    await task
+    print("Output Task : ", task.result())
+
+asyncio.run(main())
