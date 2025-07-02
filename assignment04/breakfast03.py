325649087e964c5e78fecac15e1ff36c1b06df20
V1M <nene.anantakul@gmail.com>
2025-07-02 09:31:30 +0700

    Finish breakfast fix

--- assignment04/breakfast03.py
+++ assignment04/breakfast03.py
@@ -4,14 +4,14 @@ from time import sleep, time
 
 async def make_coffee():  # 1
     print("coffee: prepare ingridients")
-    sleep(1)
+    await asyncio.sleep(1)
     print("coffee: waiting...")
     await asyncio.sleep(5)  # 2: pause, another tasks can be run
     print("coffee: ready")
 
 async def fry_eggs():  # 1
     print("eggs: prepare ingridients")
-    sleep(1)
+    await asyncio.sleep(1)
     print("eggs: frying...")
     await asyncio.sleep(3)  # 2: pause, another tasks can be run
     print("eggs: ready")
@@ -24,7 +24,9 @@ async def main() :
 
     await task1
     await task2
+
     Time = time()-start
+    
     print(f"breakfast is ready in {f"{Time:.2f}"} secs.")
 
 asyncio.run(main())
