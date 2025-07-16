6fba8289ccc20c67dfc032265ad0b93943c010f2
V1M <nene.anantakul@gmail.com>
2025-07-16 12:02:45 +0700

    POKEMON_FINISH

---
+++ assignment08/httpx/pokemon03.py
@@ -0,0 +1,23 @@
+import requests
+import time
+import asyncio
+import httpx
+
+async def fetch(name) :
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    
+    async with httpx.AsyncClient() as client :
+        response = await client.get(url)
+        data = response.json()
+        print(f"Name : {data['name'].title()}")
+        print(f"ID   : {data['id']} ") 
+        print(f"Type : {[t['type']['name'] for t in data['types']]}")
+        end = time.time()
+
+async def main() :
+    start = time.time()
+    await fetch("pikachu")
+    end = time.time()
+    print("Total Time :", round(end - start,2), "seconds")
+
+asyncio.run(main())
